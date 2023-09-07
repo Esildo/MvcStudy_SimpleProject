f79ca2e8ca2f1cc4e4cc51e440e7a82219a22cfe
Esildo <nick.ver58@gmail.com>
2023-09-07 15:54:33 +0300

    Study MVC project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        SimpleRepository Repository = SimpleRepository.SharedRepository;
+        public IActionResult Index()
+            => View(SimpleRepository.SharedRepository.Products.Where(p => p?.Price < 50));
+        [HttpGet]
+        public IActionResult AddProduct() => View(new Product());
+
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+        {
+            Repository.AddProduct(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
